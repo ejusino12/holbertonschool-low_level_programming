d6b70fcfcfd5d23f4fbc47ae3fb51d161d0185f6
ejusino12 <ejusinorodriguez@live.com>
2023-10-18 14:02:49 -0400

    lowercase reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Alphabet game
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
