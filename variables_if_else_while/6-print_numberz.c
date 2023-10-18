c7492943a6a1a368483f7f84ebfe45475a8da1e7
ejusino12 <ejusinorodriguez@live.com>
2023-10-18 13:54:12 -0400

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Alphabet game
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
