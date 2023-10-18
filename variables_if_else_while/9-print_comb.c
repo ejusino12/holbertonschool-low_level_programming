11eec0a9c4b503e82d2682e8d30095b774fbc64e
ejusino12 <ejusinorodriguez@live.com>
2023-10-18 15:18:02 -0400

    all possible combinations

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Alphabet game
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	int i;
+	putchar('0');
+	for(i = 1; i < 10; i++)
+	{
+		putchar(',');
+		putchar(' ');
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
