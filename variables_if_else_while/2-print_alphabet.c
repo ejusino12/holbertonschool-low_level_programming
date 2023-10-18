a0f801fa1a17d5405a66fc6d32f22d319a9d2253
ejusino12 <ejusinorodriguez@live.com>
2023-10-18 10:56:45 -0400

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Alphabet game
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
