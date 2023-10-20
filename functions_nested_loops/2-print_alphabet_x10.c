89a72ebef5b9e1746f2e01adc6509384037ddb22
ejusino12 <ejusinorodriguez@live.com>
2023-10-20 11:11:38 -0400

    alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - print 10 times the alphabet in lowercase
+ *print_alphabet_x10 - print 10 times the alphabet in lowercas
  */
 
 void print_alphabet_x10(void)
 {
-	int i,j;
+	int i, j;
 
-	for (i = 0; i < 10; i++){
-	for (j = 'a'; j <= 'z'; j++){
-		_putchar('a' + j);
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 'a'; j <= 'z'; j++)
+	{
+	_putchar(j);
 	}
 	_putchar('\n');
 	}
-}
-int main(void) {
-	print_alphabet_x10();
-	return 0;
 }
