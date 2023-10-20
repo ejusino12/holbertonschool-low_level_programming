d7040a02483969df7e97164442a050c7df090cf2
ejusino12 <ejusinorodriguez@live.com>
2023-10-20 11:24:43 -0400

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,12 +2,11 @@
 /**
  * print_alphabet - print the alphabet in lowercase
  */
-
-	print_alphabet(void)
+void print_alphabet(void)
 {
 	int letter;
 
-	for (letter = 'a'; letter > 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
 
