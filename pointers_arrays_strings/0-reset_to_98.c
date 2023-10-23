5bcfcc076c46513be72c8421ebef828d081e5452
ejusino12 <ejusinorodriguez@live.com>
2023-10-23 15:26:10 -0400

    return 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int and update to 98
+ *@n: character to print
+ * Return: return 98 when int
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
